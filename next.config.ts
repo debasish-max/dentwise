8fb7fd575aec480941ce730475402eadf3e37b12
debasish-max <hazarikadebasish182@gmail.com>
2026-02-06 17:05:53 +0530

    admin page added

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+        pathname: "/public/**",
+      },
     ],
   },
 };
